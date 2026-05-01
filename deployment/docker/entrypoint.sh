902de4530ffbab51c191918180cfd1b098059089
Irwan Fathurrahman <meomancer@gmail.com>
2026-05-01 16:14:50 +0700

    Add function to check session

--- deployment/docker/entrypoint.sh
+++ deployment/docker/entrypoint.sh
@@ -38,9 +38,10 @@ GATEWAY_ERPNEXT_API_URL="${GATEWAY_ERPNEXT_API_URL:-${ERPNEXT_API_URL:-https://a
 
 export erpnext_api_auth_base64=$(echo -n "${ERPNEXT_API_KEY}:${ERPNEXT_API_SECRET}" | base64 -w 0)
 export gateway_erpnext_api_url="${GATEWAY_ERPNEXT_API_URL}"
+export gateway_erpnext_host=$(echo "${GATEWAY_ERPNEXT_API_URL}" | awk -F/ '{print $3}')
 
 # Create temporary config with substituted variables in writable location
-envsubst '${erpnext_api_auth_base64} ${gateway_erpnext_api_url}' < /etc/nginx/templates/default.conf.template > /tmp/nginx/conf.d/default.conf
+envsubst '${erpnext_api_auth_base64} ${gateway_erpnext_api_url} ${gateway_erpnext_host}' < /etc/nginx/templates/default.conf.template > /tmp/nginx/conf.d/default.conf
 
 # Copy redirect rules (no envsubst needed - static config)
 cp /etc/nginx/templates/redirects.conf /tmp/nginx/redirects.conf
